e5c7b788b5e64bcc54c2dc2a2ad8051f97802640
yasser <spookyframework@gmail.com>
2020-03-27 19:21:26 +0100

    feat(config): Add firebase config file

---
+++ src/config/firebase.js
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the Jibli project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @exports
+ * @desc app's Firebase configuration
+ * @author {Yasser Ameur El Idrissi}
+ * @type {Object}
+ */
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
