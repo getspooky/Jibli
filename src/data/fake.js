85a39b215ca98c12acf9bd7ddbe03f65dbd545e7
yasser <spookyframework@gmail.com>
2020-03-27 18:53:43 +0100

    feat(data): Add fake data for test

---
+++ src/data/fake.js
@@ -0,0 +1,26 @@
+export default {
+  currentPosition: {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  },
+  deliveryList: [
+    {
+      id: 1,
+      tel: '0662134122',
+      transport: 'ford focus',
+      start: 303,
+      km: 0.33,
+      price: 0,
+    },
+    {
+      id: 2,
+      tel: '0662424233',
+      transport: 'motor',
+      start: 230,
+      km: 12,
+      price: 0,
+    },
+  ],
+};
