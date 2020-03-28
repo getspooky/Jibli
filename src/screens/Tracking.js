e465393f538f5bb66345be7a1204c9f0f00ea38a
yasser <spookyframework@gmail.com>
2020-03-28 18:34:24 +0100

    feat(screen): Save user location into firestore

--- src/screens/Tracking.js
+++ src/screens/Tracking.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import { StyleSheet, Dimensions, Text, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Delivery from '../components/Delivery';
+import Database from '../config/firebaseInit';
 const { height, width } = Dimensions.get('screen');
 import data from '../data/fake.js';
 
-export default function Tracking() {
+export default function Tracking(props) {
+  /* @var */
+  const { navigation } = props;
+
+  /**
+   * @desc Update Information collection
+   * @function
+   * @name {setDBLocation}
+   * @param {Object} location
+   * @returns {void}
+   */
+  function setDBLocation(location) {
+    Database.collection('information')
+      .where('_idUser', '==', navigation.getParam('_id'))
+      .limit(1)
+      .get()
+      .then(querySnapshot => {
+        if (querySnapshot)
+          querySnapshot.forEach(function(doc) {
+            // set current geolocation of the user.
+            doc.ref.update({
+              location,
+            });
+          });
+        else new TypeError('Account does not exists');
+      });
+  }
+
+  /**
+   * @desc Set the User geographical location
+   * @function
+   * @name {initUserGeoLocal}
+   * @returns {void}
+   */
+  function initUserGeoLocal() {
+    navigator.geolocation.getCurrentPosition(position => {
+      setDBLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.localisation} onPress={initUserGeoLocal}>
+        <Icon name="map-marker" size={20} color="#4a5568" />
+      </TouchableOpacity>
       {/*<MapView 
         region={currentPosition}
         style={styles.map} 
@@ -45,5 +100,26 @@ const styles = StyleSheet.create({
     left: 0,
     bottom: 0,
     paddingBottom: 12 * 2,
+  },
+  localisation: {
+    ...(Platform.OS === 'ios' || 'web'
+      ? {
+          shadowColor: '#323643',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 7,
+        }
+      : {
+          elevation: 1,
+        }),
+    borderRadius: 40,
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    justifyContent: 'center',
+    width: 40,
+    height: 40,
+    padding: 10,
+    alignItems: 'center',
   },
 });
