212cad4060d2b0e0ccef5abba246dbccf781fd66
yasser <spookyframework@gmail.com>
2020-03-28 19:39:11 +0100

    feat(data): Add GitHub links

---
+++ src/data/links.js
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the Jibli project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @type {string}
+ */
+export const GitHubUrl = 'https://github.com/getspooky/Jibli';
