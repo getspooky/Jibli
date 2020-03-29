0fb6a3372cb547440d4424823217311d25904c01
yasser <spookyframework@gmail.com>
2020-03-29 14:44:37 +0200

    feat(style): Add global style

---
+++ src/styles/global.js
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the Jibli project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { StyleSheet, Platform } from 'react-native';
+
+/**
+ * @exports
+ * @desc Global custom style
+ * @var {style}
+ * @types {Object}
+ */
+export const style = StyleSheet({
+  textInput: {
+    ...(Platform.OS === 'ios'
+      ? {
+          shadowColor: '#323643',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 7,
+        }
+      : {
+          elevation: 1,
+        }),
+    backgroundColor: 'white',
+    color: '#4a5568',
+    padding: 10,
+    color: '#4a5568',
+    textAlign: 'left',
+    fontSize: 13,
+    marginTop: 15,
+    width: '100%',
+  },
+  btn: {
+    ...(Platform.OS === 'ios' || 'web'
+      ? {
+          shadowColor: '#323643',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 7,
+        }
+      : {
+          elevation: 1,
+        }),
+    borderRadius: 6,
+    padding: 10,
+    marginTop: 15,
+    width: '100%',
+  },
+});
