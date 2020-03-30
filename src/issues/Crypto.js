f9161193701a146b6d7d92998141576824cba670
yasser <spookyframework@gmail.com>
2020-03-30 12:26:09 +0200

    issue: 🛑 Can't find variable: crypto

---
+++ src/issues/Crypto.js
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the Jibli project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @issue
+ * @desc Firebase Firestore: Can't find variable: crypto
+ * @github
+ * @url https://github.com/expo/expo/issues/7507
+ */
+import { decode, encode } from 'base-64';
+global.crypto = require('@firebase/firestore');
+global.crypto.getRandomValues = byteArray => {
+  for (let i = 0; i < byteArray.length; i++) {
+    byteArray[i] = Math.floor(256 * Math.random());
+  }
+};
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
